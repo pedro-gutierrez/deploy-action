bb2193f7c666889996fe0cf6d3dbdd9394707b59
Pedro Gutiérrez <pedro.gutierrez@form3.tech>
2020-11-08 22:03:38 +0100

    fix syntax

--- lib/deploy.exs
+++ lib/deploy.exs
@@ -2,29 +2,27 @@ repo = Env.ensure("GITHUB_REPOSITORY")
 secrets_name = Env.ensure("INPUT_SECRETS_NAME", "secrets")
 secrets = Env.ensure("INPUT_SECRETS") |> Base.decode64!() |> Jason.decode!()
 docker_registry = "docker.pkg.github.com"
-repo_owner, _repo_name] = String.split(repo, "/")
+[repo_owner, _repo_name] = String.split(repo, "/")
 home = "/github/home"
 workspace = "/github/workspace"
-docker_config_file = "#{home}/.docker/config.json"
 
 Shell.run("Setup Docker...", [
   "echo $INPUT_DOCKER_PASSWORD | docker login #{docker_registry} -u #{repo_owner} --password-stdin"
 ])
 
 Shell.run("Setup Kubernetes...", [
-  "mkdir -p ~/.kube",
-  "echo $INPUT_KUBECONFIG | base64 -d > ~/.kube/config"
+  "mkdir -p #{home}/.kube",
+  "echo $INPUT_KUBECONFIG | base64 -d > #{home}/.kube/config"
 ])
 
 Shell.run("Create Docker secret for Kubernetes...", [
   "kubectl delete secret docker --ignore-not-found",
-  "kubectl create secret generic docker --from-file=.dockerconfigjson=/github/home/.docker/config.json --type=kubernetes.io/dockerconfigjson"
+  "kubectl create secret generic docker --from-file=.dockerconfigjson=#{home}/.docker/config.json --type=kubernetes.io/dockerconfigjson"
 ])
 
 short_sha = Shell.run("cd #{workspace}; git rev-parse --short HEAD")
 now = Shell.run("date +%F-%T")
 version = "#{now}-#{short_sha}"
-
 
 Shell.run("Update secrets", [
   "kubectl delete secret #{secrets_name} --ignore-not-found"
